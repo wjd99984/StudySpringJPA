55d28a3cf849f92091eaa0514814e3ff672d77fc
jung tae wook <xodnr901901@naver.com>
2024-07-18 14:31:28 +0900

    fist : 첫커밋

---
+++ src/main/java/com/springjpastudy/SpringJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.springjpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaStudyApplication.class, args);
+    }
+
+}
